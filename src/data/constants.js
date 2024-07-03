e9ea7099dcf92b2ffd8241951e0490430f3f95f0
hritik <jnvbbkhritikpaswan@gmail.com>
2024-07-03 08:29:15 +0530

    fourth commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Hritik1753",
   resume:
-    "https://drive.google.com/file/d/1bd9zJwHOBD0bJ8zQEo-PabQ_9OJW5a0x/view?usp=drive_link",
+    "https://drive.google.com/file/d/113kd4OXJ54Kee5IO7fHzdXZ5pj0WZhW1/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/hritik-paswan/",
   // twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/hritik3908/",
--- src/index.js
+++ src/index.js
@@ -7,5 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+
+    {/* deploy karne package.json me jake "name" ke niche "homepage":"https//git_name.github.io/repo_name" */}
   </React.StrictMode>
 );
